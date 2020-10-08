c4dc0322d22eb72f36ae334b59fe90edbf855c33
congsonzero <zerosonnguyen@gmail.com>
2020-10-08 12:16:03 +0700

    Test

--- anyorand.py
+++ anyorand.py
@@ -1,3 +1,3 @@
-N=int(input())
-I=input().split(" ")
-print(all(int(i)>0 for i in I) and any(j==j[::-1] for j in I))
+N = int(input())
+I = input().split(" ")
+print(all(int(i) > 0 for i in I) and any(j == j[::-1] for j in I))
--- test.py
+++ test.py
@@ -1,10 +1,19 @@
-N,X=map(int,input().split())
-L=[]
-for i in range (X):
-    S=list(map(float,input().split()))
-    L.append(S)
-Y=list(zip(*L))
-for i in range(len(Y)):
-    arg=sum(Y[i])/ N
-    print(arg)
-    
+import math
+cube = lambda x: pow(x,3) # complete the lambda function 
+
+def fibonacci(n):
+    # return a list of fibonacci numbers
+    if n<=0:
+        print("Incorrect input")
+    # First Fibonacci number is 0
+    elif n==1:
+        return 0
+    # Second Fibonacci number is 1
+    elif n==2:
+        return 1
+    else:
+        return fibonacci(n-1)+fibonacci(n-2)
+
+if __name__ == '__main__':
+    n = int(input())
+    print(list(map(cube, fibonacci(n))))
